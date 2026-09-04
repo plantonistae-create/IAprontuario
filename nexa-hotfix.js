244fd2c3029ea598ff6ee5c94667072a1b5b3d24
plantonistae-create <300472959+plantonistae-create@users.noreply.github.com>
2026-09-04 00:14:57 -0400

    feat: enable auditor-only navigation and mobile radar polish

--- nexa-hotfix.js
+++ nexa-hotfix.js
@@ -5,5 +5,7 @@
     .catch(console.error)
     .finally(()=>load('./nexa-ui-v3.js?v=20260903-1')
       .catch(console.error)
-      .finally(()=>load('./nexa-radar-v4.js?v=20260903-1').catch(console.error)));
+      .finally(()=>load('./nexa-radar-v4.js?v=20260903-1')
+        .catch(console.error)
+        .finally(()=>load('./nexa-role-mobile-v4.js?v=20260904-1').catch(console.error))));
 })();
