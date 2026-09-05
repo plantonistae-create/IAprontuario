e200a160b10e0e4ed38f8c6377f45c07482c0188
plantonistae-create <300472959+plantonistae-create@users.noreply.github.com>
2026-09-05 07:58:33 -0400

    chore: invalidate cache for Radar restore v18.6.1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME="nexa-v18-6-detached-stage-20260905";
-const HOTFIX_URL="./nexa-hotfix.js?v=20260905-v186";
+const CACHE_NAME="nexa-v18-6-1-radar-restore-20260905";
+const HOTFIX_URL="./nexa-hotfix.js?v=20260905-v1861";
 const INDEX_URL="./index.html";
 
 async function injectHotfix(response){
